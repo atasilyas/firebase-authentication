43ac58834b3e82449b8feff20a07c3cdb44b12a6
atasilyas <ilyas.atas@consultant.turkcell.com.tr>
2020-05-29 00:13:59 +0300

    context data farkı cloud functions

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(); // allow admi specs
 
-exports.addAdminRole = functions.https.onCall((data, context) => {
+exports.addAdminRole = functions.https.onCall((data, context) => { // data buraya gonderdigimiz data fakat context ise o an sessiondaki user hakkındaki bilgileri tutar.Onemli
     //get user and add admin spec
     return admin.auth().getUserByEmail(data.email)
         .then(user => {
